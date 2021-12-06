1828da93592a1a4d3188c2b4cd0f0b8fbefc3728
Sebastian Krautwurst <raverjay@gmail.com>
2021-12-06 16:30:41 +0100

    initial add

---
+++ nt2aa.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# nt2aa
+# SK
+
+
+def nt2aa(nucleotides, allow_uracil=True, allow_stop_codon=False):
+    '''Returns amino acid translation for a nucleotide sequence of triplets, and None otherwise
+    '''
+
+    if not len(nucleotides)%3 == 0:
+        return None
+
+    triplets = {
+            'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+            'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+            'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+            'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+            'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+            'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+            'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+            'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+            'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+            'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+            'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+            'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+            'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+            'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+            'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+            'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'
+        }
+
+    if allow_uracil:
+        nucleotides.replace('U', 'T')
+
+    peptide = ""
+    for pos in range(0, len(nucleotides), 3):
+        codon = nucleotides[pos:pos+3]
+        peptide += triplets[codon]
+
+    # stop codon
+    if '_' in peptide:
+        return None
+    
+    return peptide
